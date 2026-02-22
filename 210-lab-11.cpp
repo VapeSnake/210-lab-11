99f8601f7176dc738737c3d600e119204202a001
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-22 12:21:24 -0800

    4th Commit: Struct Complete.

--- 210-lab-11.cpp
+++ 210-lab-11.cpp
@@ -3,15 +3,25 @@
 
 using namespace std;
 
-struct HikingTrail { //Idea: App that categorizes hiking trails by name(string), length(double), and flora(dynamic string array).
+struct HikingTrail { // Idea: App that categorizes hiking trails by name(string), length(double), and flora(dynamic string array).
     string name;
     double miles;
     string *flora;
-}
 
+    ~HikingTrail() 
+    {
+        if (flora) // Checks if pointer is null before deleting.
+            delete [] flora; // Brackets ensure every element of dynamic array is destroyed.
+        flora = nullptr; // No dangling pointers allowed.
+    }
+};
+
+void inputTrail(HikingTrail *); //
+void display(HikingTrail *);
 
 int main() 
 {
+    int numberOfTrails;
 
 
 
