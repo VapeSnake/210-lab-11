a1f65faad9e42c325dbbc774c2a2756e4a428929
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-22 12:00:43 -0800

    2nd Commit: Brainstorming

---
+++ 210-lab-11.cpp
@@ -0,0 +1,16 @@
+//COMSC-210 | Lab 11 Pointers II | Noel Mier-Luna
+#include <iostream>
+
+using namespace std;
+
+struct //Ideas: Inpatient Record System?/App that categorizes hiking trails by difficulty and wildlife&flora?
+
+
+int main() 
+{
+
+
+
+
+    return 0;
+}
